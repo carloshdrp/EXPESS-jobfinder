1a637859ca38d7a9aee8f99cd0566165689e23c4
Mentor404 <72362252+Mentor404@users.noreply.github.com>
2023-04-05 12:45:13 -0300

    Commit dia 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const exphbs = require("express-handlebars");
 const path = require("path");
 const bodyParse = require("body-parser");
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const port = 8080;
@@ -16,26 +16,38 @@ app.listen(port, function () {
 app.use(bodyParse.urlencoded({ extended: false }));
 
 // handlebars - express
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Rota padrão
 app.get("/", async (req, res) => {
-  const jobs = await prisma.job.findMany({
-    orderBy: {
-      title: 'asc'
-    }
-  }).then(() => {
-    res.json(jobs);
-    res.render('index', {jobs});
-  }).catch((error) => {
-    console.error(error);
-    res.status(500).send("Erro ao carregar dados do banco");
-  });
+  let search = req.query.jobSearch;
+  let jobs;
+  if (search) {
+    jobs = await prisma.job.findMany({
+      where: {
+        title: {
+          contains: search,
+          mode: 'insensitive',
+        }
+      },
+      orderBy: {
+        title: "asc",
+      },
+    });
+  } else {
+    jobs = await prisma.job.findMany({
+      orderBy: {
+        title: "asc",
+      },
+    });
+  }
+  // jobs para renderizar os trabalhos e search para exibir o que foi pesquisado no h2
+  res.render("index", { jobs, search });
 });
 
 // Rota Jobs
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,31 +1,77 @@
 const express = require("express");
 const router = express.Router();
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const { check, validationResult } = require("express-validator");
 
-
-router.get('/add', (req, res) => {
-    res.render('add');
+router.get("/add", (req, res) => {
+  res.render("add");
 });
 
-router.post("/add", async (req, res) => {
-        const {title, description, salary, company, email, new_job} = req.body; // new_job é enviado como string;
-        const new_job_boolean = !!(new_job); // pega o new_job e se ele não estiver vazio retorna true;
-        await prisma.job.create({
-            data: {
-                title,
-                description,
-                salary,
-                company,
-                email,
-                new_job: new_job_boolean,
-            }
-        }).then(() => {
-            res.redirect('/');
-        }).catch((error) => {
-            console.error(error);
-            res.status(500).send("Erro ao criar a nova vaga de emprego");
-        });
-});
+const errorMessages = {
+  notEmpty: "O campo não pode estar em branco.",
+  isString: "O campo precisa ser uma string.",
+  isCurrency: "O campo precisa ser um valor. Ex: 1000.00",
+  isEmail: "O campo precisa ser um email. Ex: exemple@email.com",
+};
+router.post(
+  "/add",
+  check("title")
+    .notEmpty()
+    .withMessage(errorMessages.notEmpty)
+    .isString()
+    .withMessage(errorMessages.isString)
+    .escape(),
+  check("description")
+    .notEmpty()
+    .withMessage(errorMessages.notEmpty)
+    .isString()
+    .withMessage(errorMessages.isString)
+    .escape(),
+  check("salary").isCurrency().withMessage(errorMessages.isCurrency).escape(),
+  check("company")
+    .notEmpty()
+    .withMessage(errorMessages.notEmpty)
+    .isString()
+    .withMessage(errorMessages.isString)
+    .escape(),
+  check("email")
+    .notEmpty()
+    .withMessage(errorMessages.notEmpty)
+    .isEmail()
+    .withMessage(errorMessages.isEmail)
+    .normalizeEmail(),
+  check("new_job").escape(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("add", {
+        errors: errors.array(),
+        form: req.body,
+      });
+      return;
+    }
+    const { title, description, salary, company, email, new_job } = req.body; // new_job é enviado como string;
+    const new_job_boolean = !!new_job; // pega o new_job e se ele não estiver vazio retorna true;
+    await prisma.job
+      .create({
+        data: {
+          title,
+          description,
+          salary,
+          company,
+          email,
+          new_job: new_job_boolean,
+        },
+      })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Erro ao criar a nova vaga de emprego");
+      });
+  }
+);
 
 module.exports = router;
